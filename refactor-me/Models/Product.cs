7b5ac28bc535e9f83085a3d388533c05dc9de056
Rao <rao_nt@yahoo.com>
2017-10-08 20:49:14 +0800

    First cut

---
+++ refactor-me/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data.SqlClient;
+using Newtonsoft.Json;
+using refactor_me.Models.Interface;
+
+namespace refactor_me.Models
+{
+	public class Product : IProduct
+	{
+		public Guid Id { get; set; }
+
+		public string Name { get; set; }
+
+		public string Description { get; set; }
+
+		public decimal Price { get; set; }
+
+		public decimal DeliveryPrice { get; set; }
+
+		[JsonIgnore]
+		public bool IsNew { get; set; }
+
+		public Product()
+		{
+			Id = Guid.NewGuid();
+			IsNew = true;
+		}
+	}
+}
